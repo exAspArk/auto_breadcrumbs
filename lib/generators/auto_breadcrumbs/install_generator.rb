b098b4a401e3c41ebfd83201e75e45d05497c831
Evgeny Li <exaspark@gmail.com>
2013-10-31 16:02:03 +0400

    fix install generator

--- lib/generators/auto_breadcrumbs/install_generator.rb
+++ lib/generators/auto_breadcrumbs/install_generator.rb
@@ -1,10 +1,12 @@
 module AutoBreadcrumbs
   module Generators
     class InstallGenerator < Rails::Generators::Base
+      source_root File.expand_path('../../../../', __FILE__)
+
       desc 'Copy locale files to your application.'
 
       def copy_locale
-        copy_file '../../../config/locales/en.yml', 'config/locales/auto_breadcrumbs.en.yml'
+        copy_file 'config/locales/en.yml', 'config/locales/auto_breadcrumbs.en.yml'
       end
     end
   end
