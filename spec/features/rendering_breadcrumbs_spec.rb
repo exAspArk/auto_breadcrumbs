496a875d074ab58afdf27d8bdd3bc1539d35e707
Evgeny Li <exaspark@gmail.com>
2013-11-19 11:58:06 +0400

    refactor specs a little bit

--- spec/features/rendering_breadcrumbs_spec.rb
+++ spec/features/rendering_breadcrumbs_spec.rb
@@ -1,10 +1,24 @@
 require 'spec_helper'
 require 'features/pages/breadcrumb_page'
 
-feature AutoBreadcrumbs do
+feature 'Rendering breadcrumbs' do
   let(:locale) { YAML.load(File.open(Rails.root.join('config/locales/auto_breadcrumbs.en.yml')))['en']['auto_breadcrumbs'] }
 
+  # Root page -------------------------------------------------------------------------------------
+
+  let(:root_page) { BreadcrumbPage.new(index: root_path) }
   let(:root_breadcrumbs) { [locale['root']] }
+
+  # City page -------------------------------------------------------------------------------------
+
+  let(:city_page) {
+    BreadcrumbPage.new edit: edit_city_path('id'),
+                       show: city_path('id')
+  }
+  let(:city_edit_breadcrumbs) { root_breadcrumbs + [locale['actions']['edit']] }
+  let(:city_show_breadcrumbs) { root_breadcrumbs + [locale['controllers']['cities']['show']] }
+
+  # User page -------------------------------------------------------------------------------------
 
   let(:user_page) {
     BreadcrumbPage.new index: users_path,
@@ -19,19 +33,22 @@ feature AutoBreadcrumbs do
   let(:user_dup_breadcrumbs)   { user_index_breadcrumbs + [locale['controllers']['users']['dup']] }
   let(:user_sync_breadcrumbs)  { user_index_breadcrumbs + [locale['controllers']['users']['sync']] }
 
-  let(:city_page) {
-    BreadcrumbPage.new edit: edit_city_path('id'),
-                       show: city_path('id')
-  }
-  let(:city_edit_breadcrumbs) { root_breadcrumbs + [locale['actions']['edit']] }
-  let(:city_show_breadcrumbs) { root_breadcrumbs + [locale['controllers']['cities']['show']] }
+  # -----------------------------------------------------------------------------------------------
 
-  scenario 'breadcrumbs rendering' do
-    visit root_path
-    expect(user_page.breadcrumbs).to eq(root_breadcrumbs)
+  scenario 'on root page' do
+    root_page.visit_page(:index)
+    expect(root_page.breadcrumbs).to eq(root_breadcrumbs)
+  end
 
-    # ---------------------------------------------------------------
+  scenario 'when resource without index action' do
+    city_page.visit_page(:edit)
+    expect(city_page.breadcrumbs).to eq(city_edit_breadcrumbs)
 
+    city_page.visit_page(:show)
+    expect(city_page.breadcrumbs).to eq(city_show_breadcrumbs)
+  end
+
+  scenario 'when resource with index action' do
     user_page.visit_page(:index)
     expect(user_page.breadcrumbs).to eq(user_index_breadcrumbs)
 
@@ -46,13 +63,5 @@ feature AutoBreadcrumbs do
 
     user_page.visit_page(:sync)
     expect(user_page.breadcrumbs).to eq(user_sync_breadcrumbs)
-
-    # ---------------------------------------------------------------
-
-    city_page.visit_page(:edit)
-    expect(city_page.breadcrumbs).to eq(city_edit_breadcrumbs)
-
-    city_page.visit_page(:show)
-    expect(city_page.breadcrumbs).to eq(city_show_breadcrumbs)
   end
 end
